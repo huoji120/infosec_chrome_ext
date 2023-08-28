99751db51b967b302cef0f1fff7d4258f71aa42c
Huoji's <1296564236@qq.com>
2023-08-28 23:30:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // @license      MIT
 // @name         安全人员小助手
 // @namespace    http://key08.com/
-// @version      0.5
+// @version      0.6
 // @description  安全人员小助手,安全人员必备的工具
 // @author       huoji
 // @match        *://*/*
